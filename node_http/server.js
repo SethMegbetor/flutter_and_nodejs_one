3229f4d7d84838b901578b5f0944abd567b8dd0e
“SethMegbetor” <sethmegbetor@gmail.com>
2021-12-11 12:11:22 +0000

    done with this app on

--- node_http/server.js
+++ node_http/server.js
@@ -1,13 +1,27 @@
-const express = require("express");
+// const express = require("express");
 
-const app = express();
+// const app = express();
 
-let messageJson = { message: "here we go again" };
+// let messageJson = { message: "here we go again" };
 
-app.get("/message", (request, response) => {
-  response.send(messageJson);
+// app.get("/message", (request, response) => {
+//   response.send(messageJson);
+// });
+
+// app.listen(3000, () => {
+//   console.log("listening on port 3000");
+// });
+
+//update the node js  code to this
+
+const http = require("http");
+const hostname = "127.0.0.1";
+const port = 3000;
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+  res.end("Hello World\n");
 });
-
-app.listen(3000, () => {
-  console.log("listening on port 3000");
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
