dd54ff72cd04442161c2b10d915509b82e5a0852
“SethMegbetor” <sethmegbetor@gmail.com>
2021-12-11 12:13:48 +0000

    done with this app on Saturday

--- http_app/lib/main.dart
+++ http_app/lib/main.dart
@@ -1,9 +1,9 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:app_test_http/main_screen.dart';
+import 'package:http/http.dart';
 
 void main() {
-  runApp(ProviderScope(child: MyApp()));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -12,11 +12,64 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Node Js demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          appBar: AppBar(title: const Text('Flutter client with node backend')),
+          body: const HomePage(),
         ));
-        home: const MainScreen();
   }
 }
-// 
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String serverResponse = 'Server response:';
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Align(
+          alignment: Alignment.center,
+          child: SizedBox(
+            width: 200,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                ElevatedButton(
+                  child: const Text('Make a get request to the server'),
+                  onPressed: () {
+                    _makeGetRequest();
+                  },
+                ),
+                Padding(padding: const EdgeInsets.all(8.0), child: Text(serverResponse)),
+              ],
+            ),
+          )),
+    );
+  }
+
+  _makeGetRequest() async {
+    final url = Uri.parse(_localhost());
+    Response response = await get(url);
+    setState(() {
+      serverResponse = response.body;
+    });
+  }
+
+  String _localhost() {
+    if (Platform.isAndroid) {
+      return 'http://10.0.2.2:3000';
+    }
+    else {
+      return 'http://localhost:3000';
+    }
+  }
+}
